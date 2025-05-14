39984c5ce13eace8107b19147f27aba196f8b5ec
Decko <derickrevived777@gmail.com>
2025-05-14 08:38:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+window.onload = () => {
+  const isAnimated = localStorage.getItem('buttonAnimation');
 
+  if (isAnimated === 'false') {
+    document.querySelector('.button').classList.remove('animate');
+  } else {
+    document.querySelector('.button').classList.add('animate');
+  }
+};
+
+function toggleAnimation() {
+  const button = document.querySelector('.button');
+  button.classList.toggle('animate');
+
+  if (button.classList.contains('animate')) {
+    localStorage.setItem('buttonAnimation', 'true');
+  } else {
+    localStorage.setItem('buttonAnimation', 'false');
+  }
+}
